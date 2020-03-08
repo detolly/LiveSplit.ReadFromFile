5c8b784612fc8cf25acd228464a27c006a2fc696
Thomas Hammer <h584962@stud.hvl.no>
2020-03-08 02:27:26 +0100

    asd

---
+++ UI/Components/ReadFromFileComponentFactory.cs
@@ -0,0 +1,24 @@
+﻿using LiveSplit.Model;
+using System;
+
+namespace LiveSplit.UI.Components
+{
+    public class ReadFromFileComponentFactory : IComponentFactory
+    {
+        public string ComponentName => "Read Text From File";
+
+        public string Description => "Displays any text from a file.";
+
+        public ComponentCategory Category => ComponentCategory.Information;
+
+        public IComponent Create(LiveSplitState state) => new ReadFromFileComponent(state);
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "";
+
+        public string UpdateURL => "";
+
+        public Version Version => Version.Parse("1.0.0");
+    }
+}
---
+++ UI/Components/ReadFromFileTextComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace LiveSplit.UI.Components
+{
+    public class ReadFromFileTextComponent : InfoTextComponent
+    {
+        public ReadFileComponentSettings Settings { get; set; }
+
+        public ReadFromFileTextComponent(ReadFileComponentSettings settings)
+            : base("", "")
+        {
+            Settings = settings;
+        }
+
+        public override void PrepareDraw(Model.LiveSplitState state, LayoutMode mode)
+        {
+            NameMeasureLabel.Font   =       Settings.OverrideFont1 ? Settings.Font1 : state.LayoutSettings.TextFont;
+            ValueLabel.Font         =       Settings.OverrideFont2 ? Settings.Font2 : state.LayoutSettings.TextFont;
+            NameLabel.Font          =       Settings.OverrideFont1 ? Settings.Font1 : state.LayoutSettings.TextFont;
+        }
+    }
+}
